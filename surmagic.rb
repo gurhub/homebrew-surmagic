8b8c42dc9347feb78e65b8e3f60f785c7f25c400
Muhammed Gurhan Yerlikaya <gurhub@users.noreply.github.com>
2020-12-20 17:40:09 +0300

    Update surmagic.rb

--- surmagic.rb
+++ surmagic.rb
@@ -4,8 +4,8 @@
 class Surmagic < Formula
   desc "🚀 The better way to deal with Binary Frameworks on iOS, macOS, tvOS, watchOS. Create XCFrameworks with ease."
   homepage "https://github.com/gurhub/surmagic"
-  url "https://github.com/gurhub/surmagic/archive/1.2.0.tar.gz"
-  sha256 "0ae4d3e954bfac94269beb80a874e2ffbb34f657d334697014bc4506e105698b"
+  url "https://github.com/gurhub/surmagic/archive/v1.2.1.tar.gz"
+  sha256 "7972a6de61b40901c38c3d76327f72e066a9ac48f47771bf590b86e93d6bf9b3"
   license "MIT"
 
   def install
