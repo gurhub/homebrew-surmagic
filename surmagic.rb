741c7573f881d0d1a1017eb3a2858c87fefb555f
Muhammed Gurhan Yerlikaya <gurhub@users.noreply.github.com>
2022-02-06 22:09:03 +0300

    Updated to the Surmagic version 1.2.4
    Added a New Feature: Final Actions
    For now, we've only 1 option for final action -> open directory
    More to come later
    Feel free to suggest something useful

--- surmagic.rb
+++ surmagic.rb
@@ -4,8 +4,8 @@
 class Surmagic < Formula
   desc "🚀 Create XCFrameworks with ease! A Command Line Tool to create XCFrameworks for multiple platforms at one shot! The better way to deal with XCFrameworks for iOS, Mac Catalyst, tvOS, macOS, and watchOS."
   homepage "https://github.com/gurhub/surmagic"
-  url "https://github.com/gurhub/surmagic/archive/refs/tags/1.2.3.tar.gz"
-  sha256 "c2d67f8af1c594e777f9abccf56b449813642e362b33027f4a8d55ec25cefc0f"
+  url "https://github.com/gurhub/surmagic/archive/refs/tags/1.2.4.tar.gz"
+  sha256 "6469be42d0f456d3d0d188aac4540aac11378ba949e09a1cb57c98ab3ada1e39"
   license "MIT"
 
   def install
