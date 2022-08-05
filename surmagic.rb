02ade9b10caf7b7d04b678ce2613bd600722309a
gurhub <gurhanyerlikaya@gmail.com>
2022-08-05 12:14:16 +0000

    Updated version v1.2.7

--- surmagic.rb
+++ surmagic.rb
@@ -4,8 +4,8 @@
 class Surmagic < Formula
   desc "🚀 Create XCFrameworks with ease! A Command Line Tool to create XCFrameworks for multiple platforms at one shot! The better way to deal with XCFrameworks for iOS, Mac Catalyst, tvOS, macOS, and watchOS."
   homepage "https://github.com/gurhub/surmagic"
-  url "https://github.com/gurhub/surmagic/archive/refs/tags/v1.2.6.tar.gz"
-  sha256 "052d5060393f6b28943436947867d820f19aa588523f6022d5c752cc75a4803b"
+  url "https://github.com/gurhub/surmagic/archive/refs/tags/v1.2.7.tar.gz"
+  sha256 "106d10fb693afd2f862bf58a3e19572a18ff955f0e75f3a93e1f4cc4ae9864e6"
   license "MIT"
 
   def install
